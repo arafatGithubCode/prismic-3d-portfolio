9a38404037a75550ffb8fb52c1278966acb25619
arafatGithubCode <md489288@gmail.com>
2024-07-07 18:05:11 +0600

    add texture lighter of bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,8 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
+        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
       </body>
     </html>
   );
